811722c7d718efdba7d86d917ac1ce93536501dc
Siriusq <eargon.skyle@gmail.com>
2021-11-06 04:43:56 +0000

    Add Rigid & Gravity now works & TurnLeft TurnRight Now Works & Animation Need Fixes

--- Assets/Script/Movement.cs
+++ Assets/Script/Movement.cs
@@ -4,14 +4,14 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
+    Animator dogBehaviour;
+
     public ArrayList received = new ArrayList();
     bool readin = false;
+    bool finished = false;
 
-    Vector3 up = Vector3.zero;
-    Vector3 right = new Vector3(0, 90, 0);
-    Vector3 down = new Vector3(0, 180, 0);
-    Vector3 left = new Vector3(0, 270, 0);
-    Vector3 currentDirection = Vector3.zero;
+    Vector3 leftRotation = new Vector3(0, -90, 0);
+    Vector3 rightRotation = new Vector3(0, 90, 0);
 
     Vector3 nextPos;
     Vector3 destination;
@@ -19,12 +19,15 @@ public class Movement : MonoBehaviour
 
     float speed = 500f;
     float rayLength = 1f;
+    Vector3 currentVelocity = Vector3.zero;
+    float smoothTime = 0.3F;
 
     bool canMove;
 
     void Start()
     {
         received = null;
+        dogBehaviour = GetComponent<Animator>();
     }
 
     void Update()
@@ -34,30 +37,49 @@ public class Movement : MonoBehaviour
             StartCoroutine(Test());
             readin = true;
         }
+
+        if (finished)
+        {
+            StopCoroutine(Test());
+        }
+
     }
 
     IEnumerator Test()
     {
-        Debug.Log("aaa");
+        Debug.Log("Start");
         foreach (string s in received)
         {
             Move(s);
-            Debug.Log(s);
-            yield return new WaitForSeconds(1);            
+            Debug.Log(s);            
+            yield return new WaitForSeconds(1);
+            dogBehaviour.SetBool("isMove", false);
         }
-        yield return new WaitForSeconds(1);
+        finished = true;
+        yield return new WaitForSeconds(1);        
     }
 
     public void Move(string x)
     {
         //transform.position = Vector3.MoveTowards(transform.position, destination, speed * Time.deltaTime);
-
-
-        
+                
         if (x.Equals("MoveForward"))
         {
+            dogBehaviour.SetBool("isMove", true);
             this.transform.position = Vector3.MoveTowards(this.transform.position, this.transform.position + transform.forward, speed * Time.deltaTime);
+            //Debug.Log(Time.deltaTime);
+            //this.transform.position = Vector3.SmoothDamp(this.transform.position, this.transform.position + transform.forward, ref currentVelocity, 0.1f);
+            //this.transform.position = Vector3.Lerp(this.transform.position, this.transform.position + transform.forward, speed * Time.deltaTime);
         }
+        else if (x.Equals("TurnLeft"))
+        {
+            this.transform.Rotate(leftRotation);
+        }
+        else if (x.Equals("TurnRight"))
+        {
+            this.transform.Rotate(rightRotation);
+        }
+               
     }
 
     public void GetCode(ArrayList codes)
