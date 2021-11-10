75ae02a9589fcf43396fc130f447ddcc2ef2a511
Siriusq <eargon.skyle@gmail.com>
2021-11-10 18:09:25 +0000

    Scene Transation Animation Added Successful!

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -7,6 +7,8 @@ using UnityEngine.SceneManagement;
 public class GameManager : MonoBehaviour
 {
     static GameManager manager;
+    public Animator transAnimation;
+
     void Awake()
     {
         if(manager != null)
@@ -18,7 +20,7 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -62,5 +64,26 @@ public class GameManager : MonoBehaviour
     public void LevelComplete()//通关页面
     {
 
+    }
+
+    public void Play()//Main Menu 上面的 Play 按钮，按下后切换到关卡选择
+    {
+        SceneManager.LoadScene("Select");
+    }
+
+
+    public void Continue()//加载下一关
+    {
+        int tempIndex = SceneManager.GetActiveScene().buildIndex + 1;
+        Debug.Log(tempIndex);
+        StartCoroutine(NextLevel(SceneManager.GetActiveScene().buildIndex + 1));//buildingIndex是build setting里面场景的序号
+        //StopCoroutine(NextLevel(tempIndex));
+    }
+
+    IEnumerator NextLevel(int index)//携程加载下一个场景，可以用在下一关按钮，也可以用在主菜单切换到关卡选择上
+    {
+        transAnimation.SetTrigger("Start");
+        yield return new WaitForSeconds(0.7f);
+        SceneManager.LoadScene(index);
     }
 }
